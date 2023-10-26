e72e4250945d368d4859ce008bbf8bd2f41b34b7
Joseph Troughton <146139561+Karmakze@users.noreply.github.com>
2023-10-26 14:53:08 +0100

    Lab 5-1

--- Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
+++ Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main()
 {
+	ofstream OutputStream;
+	OutputStream.open("tables.txt");
+	if (OutputStream.is_open() == false)
+	{
+		cout << "error could not open file" << endl;
+		return -1;
+	}
 	for (unsigned int r = 2; r <= 12; r++) {
 		for (unsigned int c = 2; c <= 12; c++) {
-			cout << r * c << "\t";
+			 OutputStream<< r * c << "\t";
 		}
-		cout << endl;
+		OutputStream << endl;
 	}
-	cout << endl;
+	OutputStream << endl;
+	OutputStream.close();
 
 	return 0;
 }
